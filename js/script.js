d97161a68d6f65bb5811614e9065ba1d29b098a2
Thomricco <tommasoricco@hotmail.it>
2021-12-13 15:59:13 +0100

    inizio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//creare una lista itenti
+
+// chiedere email all'utente
+
+// se lutente è nella lista dare l'ok
+
+
+
+// creare due giocatori
+
+//Generare un numero random da 1 a 6 per ogni giocatore
+
+// il numero più alto vince
